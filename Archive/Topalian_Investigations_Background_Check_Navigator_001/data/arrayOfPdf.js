6d0204b19bad4197521cf64a692765d65ad84bd4
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2023-02-19 20:14:30 -0500

    Add files via upload

---
+++ Archive/Topalian_Investigations_Background_Check_Navigator_001/data/arrayOfPdf.js
@@ -0,0 +1,55 @@
+// Dedicated to God the Father
+
+// All Rights Reserved Christopher Topalian Copyright 2000-2023
+
+// https://github.com/ChristopherTopalian
+
+// arrayOfPdf.js
+
+let arrayOfPdf = [
+{
+    name: "Doe, John",
+    url: "pdf/doe_john.pdf"
+},
+
+{
+    name: "Doe, Jane",
+    url: "pdf/doe_jane.pdf"
+},
+
+{
+    name: "Doe, Ronald",
+    url: "pdf/doe_ronald.pdf"
+},
+
+{
+    name: "Doe, Beth",
+    url: "pdf/doe_beth.pdf"
+},
+
+{
+    name: "Doe, Evan",
+    url: "pdf/doe_evan.pdf"
+},
+
+{
+    name: "Doe, Tabitha",
+    url: "pdf/doe_tabitha.pdf"
+},
+
+{
+    name: "Doe, Thomas",
+    url: "pdf/doe_thomas.pdf"
+},
+
+{
+    name: "Doe, Grace",
+    url: "pdf/doe_grace.pdf"
+},
+
+{
+    name: "Doe, Nathan",
+    url: "pdf/doe_nathan.pdf"
+},
+
+];
